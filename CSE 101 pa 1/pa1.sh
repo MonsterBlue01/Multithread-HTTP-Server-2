ef2bd9768ad5bf02583b3e4a0459caa591c58eb8
Dongjing Wang <dwang114@unix3.lt.ucsc.edu>
2022-04-06 15:10:17 -0700

    2022/04/06 15:10

--- CSE 101 pa 1/pa1.sh
+++ CSE 101 pa 1/pa1.sh
@@ -9,8 +9,8 @@ EXE_ALL=( pa1-func-check.sh pa1-unit-check.sh pa1-make-check.sh )
 EXE_RANGE=$((${#EXE_ALL[*]} - 1))
 echo $EXE_RANGE
 for i in $(seq 0 $EXE_RANGE); do
-    curl $SRCDIR/${EXE_ALL[i]} > ${EXE_ALL[i]}
-    chmod +x ${EXE_ALL[i]}
-    ./${EXE_ALL[i]}
-    rm -f ${EXE_ALL[i]}
+  curl $SRCDIR/${EXE_ALL[i]} > ${EXE_ALL[i]}
+  chmod +x ${EXE_ALL[i]}
+  ./${EXE_ALL[i]}
+  rm -f ${EXE_ALL[i]}
 done
