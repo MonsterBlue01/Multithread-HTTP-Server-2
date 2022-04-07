5e8b54d96e62b8781a34529a03d4d9168ebe7919
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-04-07 12:56:50 -0700

    2022/04/07 12:56

--- CSE 101 pa 2/GraphTest.c
+++ CSE 101 pa 2/GraphTest.c
@@ -5,7 +5,7 @@
 
 int main() {
     Graph new = newGraph(3);
-    if (new->neighbor[1] == NULL) {
+    /*if (new->neighbor[1] == NULL) {
         new->neighbor[1] = newList();
     }
 
@@ -14,6 +14,7 @@ int main() {
     }
 
     append(new->neighbor[1], 2);
-    append(new->neighbor[2], 1);
+    append(new->neighbor[2], 1);*/
+    addEdge(new, 1, 2);
     printf("%d\n", new->neighbor[1]->front->data);
 }
