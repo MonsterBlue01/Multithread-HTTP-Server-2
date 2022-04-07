fb5eee7864006ec44fd9ebb84c0cfbe0319cf569
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-04-07 13:32:17 -0700

    2022/04/07 13:32

--- CSE 101 pa 2/question.sh
+++ CSE 101 pa 2/question.sh
@@ -4,3 +4,4 @@
 #4. Should we use append twice? (bi-direction graph?)
 #5. How to use 'n' that in the newGraph?
 #6. Do we need to allocate the space for neighbor in newGraph? or allocate it when we need that later?
+#7. Why add edge and add arch should use two different functions? Are they doing same things?
