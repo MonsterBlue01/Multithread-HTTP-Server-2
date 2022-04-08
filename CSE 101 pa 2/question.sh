5644c179873be99b197049b1175da8d8fc6d1f0e
monstblu <wdsrs020808@gmail.com>
2022-04-07 18:19:35 -0700

    2022/04/07 18:17

--- CSE 101 pa 2/question.sh
+++ CSE 101 pa 2/question.sh
@@ -1,8 +1,6 @@
 #1. So basically, getOrder() is the way to track the Graph?
-#2. Each node only has one parent?
-#3. Each node has unique value?
 #5. How to use 'n' that in the newGraph?
 #6. Do we need to allocate the space for neighbor in newGraph? or allocate it when we need that later?
-#8. Understanding about BFS pseudocode
 #9. Debug
 #10. The format of print
+#11. How to know if BFS() is excuted
