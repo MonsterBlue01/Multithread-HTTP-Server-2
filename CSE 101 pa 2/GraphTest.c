b7c57c4cfa75aebb65f6957f3bbfab37b43c9666
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-04-11 11:46:15 -0700

    2022/04/11 11:46

--- CSE 101 pa 2/GraphTest.c
+++ CSE 101 pa 2/GraphTest.c
@@ -5,6 +5,38 @@
 #include "Graph.h"
 
 int main() {
+    Graph A = newGraph;
+
+    for (uint8_t i = 1; i <= 100; i++)
+        if (getDist(A, i) != INF) {
+            return 1;
+        }
+    addArc(A, 64, 4);
+    addArc(A, 64, 3);
+    addArc(A, 42, 2);
+    addArc(A, 2, 64);
+    addArc(A, 4, 2);
+    addArc(A, 3, 42);
+    BFS(A, 64);
+    if (getDist(A, 64) != 0) {
+        return 2;
+    }
+    if (getDist(A, 2) != 2) {
+        return 3;
+    }
+    BFS(A, 4);
+    if (getDist(A, 42) != 4) {
+        return 4;
+    }
+    if (getDist(A, 43) != INF) {
+        return 5;
+    }
+    BFS(A, 99);
+    if (getDist(A, 64) != INF) {
+        return 6;
+    }
+    return 0;
+
     /*Graph G = newGraph(15);
     addEdge(G, 1, 2);
     addEdge(G, 1, 7);
