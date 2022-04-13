2b4c5c5c66bdeafdb47b95e1ca74e696aed30bdf
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-04-12 20:14:04 -0700

    a

--- CSE 101 pa 3/GraphTest.c
+++ CSE 101 pa 3/GraphTest.c
@@ -11,6 +11,7 @@ int main() {
     addArc(G, 2, 1);
     addArc(G, 1, 1);
     printGraph(stdout, G);
+    
     //printf("The size: %d\n", G->size);
     //printf("The graph: %d\n", G->order);
     return 0;
