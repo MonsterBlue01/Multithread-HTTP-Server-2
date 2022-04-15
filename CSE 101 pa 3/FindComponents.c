e801d7c64d29997abd88e2860eff29631a09a33b
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-04-14 21:03:18 -0700

    2022/04/14 21:03

--- CSE 101 pa 3/FindComponents.c
+++ CSE 101 pa 3/FindComponents.c
@@ -10,21 +10,75 @@ int main(int argc, char **argv) {
         exit(1);
     }
     FILE *fp = NULL;
-    int n1, n2;
+    FILE *fp1 = NULL;
+    fp = fopen(argv[1], "r");
+    fp1 = fopen(argv[2], "w+");
+    if ((fp == NULL) || (fp1 == NULL)) {
+        fprintf(stderr, "File Error: invalid files input!\n");
+        exit(1);
+    }
+    int a, b;
     int num;
+    int snum = 0;
+    int in = 0;
+
     fscanf(fp, "%d", &num);
     Graph G = newGraph(num);
+    List L = newList();
+    List Lt = newList();
+    
+    while (fscanf(fp, "%d", &a) != EOF) {
+        fscanf(fp, "%d", &b);
+        addArc(G, a, b);
+    }
+    Graph Gt = transpose(G);
+    DFS(G, L);
+    DFS(Gt, Lt);
+    
+    fprintf(fp1, "Adjacency list representation of G:\n");
+    printGraph(fp1, G);
+    fprintf(fp1, "\n");
 
-    while (fscanf(fp, "%d", &n1) != EOF) {
-        fscanf(fp, "%d", &n2);
-        if ((n1 == 0) && (n2 == 0)) {
-            break;
+    for (int i = 1; i <= G->order; i++) {
+        if (Gt->parent[i] == 0) {
+            printf("Im: %d\n", i);
+            snum++;
         }
-        addEdge(G, n1, n2);
+    }
+    int array[snum];
+
+    for (int i = 1; i <= G->order; i++) {
+        if (Gt->parent[i] == 0) {
+            array[in++] = i;
+        }
     }
 
-    printGraph(stdout, G);
+    fprintf(fp1, "\nG contains %d strongly connected components:", snum);
+    int m = 0;
+    int n = 0;
 
-    fp = fopen(argv[1], "r");
+    while (m != snum) {
+        while (true) {
+            moveBack(Lt);
+            while (Lt->cursor->data != array[m]) {
+                movePrev(Lt);
+            }
+            fprintf(fp1, "\nComponent %d: ", ++n);
+            while (index(Lt) >= 0) {
+                fprintf(fp1, "%d ", Lt->cursor->data);
+                movePrev(Lt);
+                if (m != 0) {
+                    if (Lt->cursor->data == array[m - 1]) {
+                        break;
+                    }
+                }
+            }
+            break;
+        }
+        m++;
+    }
+    printList(stdout, Lt);
+    fprintf(fp1, "\n");
     fclose(fp);
+    fclose(fp1);
 }
