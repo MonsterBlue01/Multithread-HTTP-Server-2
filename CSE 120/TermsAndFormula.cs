25c052a6048f9891ed5a8949f8a18824765bd20b
Blue Monster <73154603+MonsterBlue01@users.noreply.github.com>
2022-04-19 22:08:21 -0700

    2022/04/19 22:00

--- CSE 120/TermsAndFormula.cs
+++ CSE 120/TermsAndFormula.cs
@@ -1,15 +1,23 @@
-// Terms:
-// 1. Response time: Also called execution time. The total time required for the computer to complete a task, including disk accesses, memory accesses, 
-//     I/O activities, operating system overhead, CPU execution time, and so on.
-// 2. Throughput: Also called bandwidth. Another measure of performance, it is the number of tasks completed per unit time.
-// 3. CPU execution time: Also called CPU time. The actual time the CPU spends computing for a specific task.
-// 4. User CPU time: The CPU time spent in a program itself.
-// 5. System CPU time: The CPU time spent in the operating system performing tasks on behalf of the program.
-// 6. Clock Cycle: Also called tick, clock tick, clock period, clock, or cycle. The time for one clock period, usually of the processor clock, which runs at a 
-//     constant rate.
-// 7. Clock Period: The length of each clock cycle.
+Terms:
+1. Response time: Also called execution time. The total time required for the computer to complete a task, including disk accesses, memory accesses, 
+    I/O activities, operating system overhead, CPU execution time, and so on.
+2. Throughput: Also called bandwidth. Another measure of performance, it is the number of tasks completed per unit time.
+3. CPU execution time: Also called CPU time. The actual time the CPU spends computing for a specific task.
+4. User CPU time: The CPU time spent in a program itself.
+5. System CPU time: The CPU time spent in the operating system performing tasks on behalf of the program.
+6. Clock Cycle: Also called tick, clock tick, clock period, clock, or cycle. The time for one clock period, usually of the processor clock, which runs at a 
+    constant rate.
+7. Clock Period: The length of each clock cycle.
+8. Clock Cycles Per Instruction (CPI): Average number of clock cycles per instruction for a program or program fragment.
+9. Instruction mix: A measure of the dynamic frequency of instructions across one or many programs.
 
-// Formulas:
-// 1. Performance = 1 / Excution time
-// 2. CPU execution time = CPU clock cycles for a program for a program * Clock cycle time
-// 3. CPU execution time for a program = CPU clock cycles for a program / Clock rate
+Formulas:
+1. Performance = 1 / Excution time
+2. CPU execution time = CPU clock cycles for a program for a program * Clock cycle time
+3. CPU execution time for a program = CPU clock cycles for a program / Clock rate
+4. CPU clock cycles = Instructions for a program * Average clock cycles per instruction
+5. CPU time = Instructioncount * CPI * Clock cycle time
+6. CPU time = Instruction count * CPI / Clock rate
+7. CPU clock cycles = CPI * C
+8. Power ratio = Power_new / Power_old
+9. Power = Capacitive load * Voltage ^ 2 * Frequency Switched
