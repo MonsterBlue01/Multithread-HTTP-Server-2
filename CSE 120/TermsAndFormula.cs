deb17cddc42380823009337f86a5a17b9724a054
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-04-19 21:01:58 -0700

    2022/04/19 21:00

---
+++ CSE 120/TermsAndFormula.cs
@@ -0,0 +1,15 @@
+// Terms:
+// 1. Response time: Also called execution time. The total time required for the computer to complete a task, including disk accesses, memory accesses, 
+//     I/O activities, operating system overhead, CPU execution time, and so on.
+// 2. Throughput: Also called bandwidth. Another measure of performance, it is the number of tasks completed per unit time.
+// 3. CPU execution time: Also called CPU time. The actual time the CPU spends computing for a specific task.
+// 4. User CPU time: The CPU time spent in a program itself.
+// 5. System CPU time: The CPU time spent in the operating system performing tasks on behalf of the program.
+// 6. Clock Cycle: Also called tick, clock tick, clock period, clock, or cycle. The time for one clock period, usually of the processor clock, which runs at a 
+//     constant rate.
+// 7. Clock Period: The length of each clock cycle.
+
+// Formulas:
+// 1. Performance = 1 / Excution time
+// 2. CPU execution time = CPU clock cycles for a program for a program * Clock cycle time
+// 3. CPU execution time for a program = CPU clock cycles for a program / Clock rate
