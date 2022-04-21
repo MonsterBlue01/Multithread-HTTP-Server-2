6e591804039e68d92bcc1e3dc3420b29e516166e
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-04-20 20:30:27 -0700

    2022/04/20 20:30

--- CSE 101 pa 4/MatrixTest.c
+++ CSE 101 pa 4/MatrixTest.c
@@ -18,7 +18,23 @@ int main() {
     double c = 3.0;
     
     changeEntry(M, 1, 2, 2.0);
-    changeEntry(M, 1, 3, 3.0);
+    // changeEntry(M, 1, 3, 3.0);
+    Entry new = (Entry)malloc(sizeof(EntryObj));
+    new->column = 3;
+    new->num = 3.0;
+    Node tmp = M->row[1]->front;
+    if (tmp == NULL) {
+        append(M->row[1], new);
+    } else {
+        if (3 < ((Entry)(M->row[1]->front->data))->column) {
+            prepend(M->row[1], new);
+        } else {
+            while (tmp != NULL) {
+                printf("%d\n", ((Entry)(M->row[1]->front->next->data))->column);
+                tmp = tmp->next;
+            }
+        }
+    }
 
     if (M->row[1]->front != NULL) {
         printf("%f\n", ((Entry)(M->row[1]->front->next->data))->num);
