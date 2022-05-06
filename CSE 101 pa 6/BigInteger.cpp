fbf28ad296262f828f3b0257db4387a9fbb86f08
Dongjing Wang <dwang114@unix6.lt.ucsc.edu>
2022-05-05 21:23:50 -0700

    5/5/22 9:23 PM

--- CSE 101 pa 6/BigInteger.cpp
+++ CSE 101 pa 6/BigInteger.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 #include<stdexcept>
+#include<bits/stdc++.h>
 
 #include"BigInteger.h"
 #include"List.h"
@@ -14,6 +15,11 @@ BigInteger::BigInteger(){
 BigInteger::BigInteger(std::string s){
     signum = 0;
 
+    if (s == "0") {
+        digits.insertAfter(0);
+        return;
+    }
+
     std::string par;
     List L;
     std::string n = s.substr(0, 1);
@@ -24,31 +30,137 @@ BigInteger::BigInteger(std::string s){
         n = s;
         signum = 1;
     }
-    for (int i = 0; i < n.size(); i += 9) {
-        par = n.substr(i, 9);
-        int tmp = stoi(par);
-        this->digits.insertBefore(tmp);
+    for (int i = s.length() - 9; i > 0; i -= 9) {
+        L.insertAfter(stoi(s.substr(i, 9)));
     }
+    if (s.length() % 9 != 0) {
+        L.insertAfter(stoi(s.substr(0, s.length() % 9)));
+    } else {
+        L.insertAfter(stoi(s.substr(0, 9)));
+    }
+
+    this->digits = L;
+}
+
+BigInteger::BigInteger(const BigInteger& N) {
+    this->signum = N.signum;
+    this->digits = N.digits;
+}
+
+int BigInteger::sign() const{
+    return signum;
+}
+
+BigInteger BigInteger::add(const BigInteger& N) const {
+    BigInteger b1 = *this;
+    BigInteger b2 = N;
+    BigInteger res;
+    int m = 0;
+    int n1, n2, j = 0;
+    
+    b1.digits.moveBack();
+    b2.digits.moveBack();
+
+    if ((this->signum == 1) && (N.signum == 1)) {
+        res.signum = 1;
+        while ((b1.digits.position() != 0) || (b2.digits.position() != 0)) {
+            n1 = 0;
+            n2 = 0;
+            if (b1.digits.position() != 0) {
+                n1 = b1.digits.peekPrev();
+                b1.digits.movePrev();
+            }
+            if (b2.digits.position() != 0) {
+                n2 = b2.digits.peekPrev();
+                b2.digits.movePrev();
+            }
+
+            if (j == 1) {
+                if (n1 + n2 + 1 >= 1000000000) {
+                    std::cout << "sum: " << n1 + n2 + 1 - 1000000000 << std::endl;
+                } else {
+                    std::cout << "sum: " << n1 + n2 + 1 << std::endl;
+                    j = 0;
+                }
+            } else {
+                if (n1 + n2 + 1 >= 1000000000) {
+                    std::cout << "sum: " << n1 + n2 - 1000000000 << std::endl;
+                    j = 1;
+                } else {
+                    std::cout << "sum: " << n1 + n2 << std::endl;
+                }
+            }
+        }
+    }
+    return res;
+}
+
+BigInteger BigInteger::sub(const BigInteger& N) const {
+    BigInteger o = *this;
+    BigInteger t = N;
+    o.digits.moveBack();
+    t.digits.moveBack();
+    int j;
+    while ((o.digits.position() != 0) || (t.digits.position() != 0)) {
+        int n1 = 0;
+        int n2 = 0;
+        if (o.digits.position() != 0) {
+            std::cout << "out" << std::endl;
+            n1 = o.digits.peekPrev();
+            o.digits.movePrev();
+        }
+        if (t.digits.position() != 0) {
+            std::cout << "out" << std::endl;
+            n2 = t.digits.peekPrev();
+            t.digits.movePrev();
+        }
+        std::cout << "n1: " << n1 << std::endl;
+        std::cout << "n2: " << n2 << std::endl;
+        if (j == -1) {
+            if (n1 - n2 - 1 < 0) {
+                std::cout << "diff: " << n1 - n2 - 1 + 1000000000 << std::endl;
+            } else {
+                std::cout << "diff: " << n1 - n2 - 1 << std::endl;
+                j = 0;
+            }
+        } else {
+            if (n1 - n2 < 0) {
+                std::cout << "diff: " << n1 - n2 + 1000000000 << std::endl;
+                j = -1;
+            } else {
+                std::cout << "diff: " << n1 - n2 << std::endl;
+            }
+        }
+    }
+    return N;
 }
 
 std::string BigInteger::to_string(){
-    std::string tmp = "";
-    List L;
-    if (this->digits == L) {
-        return tmp;
+    std::string s = "";
+    for (this->digits.moveBack(); this->digits.position() != 0; this->digits.movePrev()) {
+        std::string t = std::to_string(this->digits.peekPrev());
+        if (this->digits.position() != 1) {
+            if (this->digits.peekPrev() == 0) {
+                t = "000000000";
+            } else {
+                for (int i = 0; i < 8 - (int)log10(this->digits.peekPrev()); i++) {
+                    t = "0" + t;
+                }
+            }
+        }
+        s = t + s;
     }
-    if (this->signum == -1) {
-        tmp = "-" + tmp;
-    }
-    for (this->digits.moveFront(); this->digits.position() != this->digits.length(); this->digits.moveNext()) {
-        int num = this->digits.peekNext();
-        std::string s = std::to_string(num);
-        tmp = tmp + s;
-    }
-    return tmp;
+    return s;
 }
 
 std::ostream& operator<< ( std::ostream& stream, BigInteger N ) {
     return stream << N.BigInteger::to_string();
 }
 
+BigInteger operator+( const BigInteger& A, const BigInteger& B ) {
+    return A.BigInteger::add(B);
+}
+
+BigInteger operator-( const BigInteger& A, const BigInteger& B ) {
+    return A.BigInteger::sub(B);
+}
--- CSE 101 pa 6/BigIntegerClient.cpp
+++ CSE 101 pa 6/BigIntegerClient.cpp
@@ -12,106 +12,92 @@
 using namespace std;
 
 int main(){
-    string par;
-    List L;
-    string a = "1234567891011121314151617181920212223242526272829303132";
-    string n = a.substr(0, 1);
-    if (n == "-") {
-        n = a.substr(1);
-    } else {
-        n = a;
-    }
-    for (int i = 0; i < n.size(); i += 9) {
-        par = n.substr(i, 9);
-        int tmp = stoi(par);
-        L.insertBefore(tmp);
-    }
+    string s1 = "9128734667000004389234563456345619187236478";
+    //string s2 = "-330293847502398475";
+    string s3 = "9876545439000000000000000200000000000006543654365346534";
+    //string s4 = "9876545439000000000000000100000000000006543654365346534";
+    //string s5 = "98765454390000000000000000000000000000065436543";
+    //string s6 = "9876545439000000000000000000000000000006543";
 
-    cout << BigInteger(a) << endl;
-    return 0;
-/*
-   string s1 = "9128734667000004389234563456345619187236478";
-   //string s2 = "-330293847502398475";
-   string s3 = "9876545439000000000000000200000000000006543654365346534";
-   //string s4 = "9876545439000000000000000100000000000006543654365346534";
-   //string s5 = "98765454390000000000000000000000000000065436543";
-   //string s6 = "9876545439000000000000000000000000000006543";
+    BigInteger N;
+    BigInteger A = BigInteger(s1);
+    BigInteger B = BigInteger(s3);
 
-   //BigInteger N;
-   BigInteger A = BigInteger(s1);
-   BigInteger B = BigInteger(s3);
+    cout << endl;
 
-   cout << endl;
+    cout << "A's signum = " << A.sign() << endl;
+    cout << "A = " << A << endl;
+    cout << "B's signum = " << B.sign() << endl;
+    cout << "B = " << B << endl;
+    cout << endl;
 
-   cout << "A = " << A << endl;
-   cout << "B = " << B << endl;
-   cout << endl;
+    // BigInteger C = B-A;
+    // cout << "C: " << C << endl;
 
-   BigInteger C = A+B;
-   BigInteger D = B+A;
-   BigInteger E = A-A;
-   BigInteger F = B-A;
-   BigInteger G = A*B;
-   BigInteger H = B*A;
-   BigInteger I = C*E;
+//    BigInteger D = B+A;
+//    BigInteger E = A-A;
+//    BigInteger F = B-A;
+//    BigInteger G = A*B;
+//    BigInteger H = B*A;
+//    BigInteger I = C*E;
 
-   cout << "(A==B) = " << ((A==B)?"True":"False") << endl;
-   cout << "(A<B)  = " << ((A<B)? "True":"False") << endl;
-   cout << "(A<=B) = " << ((A<=B)?"True":"False") << endl;
-   cout << "(A>B)  = " << ((A>B)? "True":"False") << endl;
-   cout << "(A>=B) = " << ((A>=B)?"True":"False") << endl << endl;
+//    cout << "(A==B) = " << ((A==B)?"True":"False") << endl;
+//    cout << "(A<B)  = " << ((A<B)? "True":"False") << endl;
+//    cout << "(A<=B) = " << ((A<=B)?"True":"False") << endl;
+//    cout << "(A>B)  = " << ((A>B)? "True":"False") << endl;
+//    cout << "(A>=B) = " << ((A>=B)?"True":"False") << endl << endl;
 
-   cout << "C = " << C << endl;
-   cout << "D = " << D << endl;
-   cout << "(C==D) = " << ((C==D)?"True":"False") << endl;
-   cout << "E = " << E << endl;
-   cout << "F = " << F << endl;
-   cout << "G = " << G << endl;
-   cout << "H = " << H << endl;
-   cout << "(G==H) = " << ((G==H)?"True":"False") << endl;
-   cout << "I = " << I << endl << endl;
+//    cout << "C = " << C << endl;
+//    cout << "D = " << D << endl;
+//    cout << "(C==D) = " << ((C==D)?"True":"False") << endl;
+//    cout << "E = " << E << endl;
+//    cout << "F = " << F << endl;
+//    cout << "G = " << G << endl;
+//    cout << "H = " << H << endl;
+//    cout << "(G==H) = " << ((G==H)?"True":"False") << endl;
+//    cout << "I = " << I << endl << endl;
 
-   cout << endl;
+//    cout << endl;
 
-   A += B;
-   B -= C;
-   C *= D;
-   cout << "A = " << A << endl;
-   cout << "B = " << B << endl;
-   cout << "C = " << C << endl;
-   cout << endl;
+//    A += B;
+//    B -= C;
+//    C *= D;
+//    cout << "A = " << A << endl;
+//    cout << "B = " << B << endl;
+//    cout << "C = " << C << endl;
+//    cout << endl;
 
-   cout << A*B*C*D*F*G*H << endl << endl;
+//    cout << A*B*C*D*F*G*H << endl << endl;
 
-   cout << endl;
+//    cout << endl;
 
-   // test exceptions
-   try{
-      BigInteger J = BigInteger("");
-   }catch( std::invalid_argument& e ){
-      cout << e.what() << endl;
-      cout << "   continuing without interruption" << endl;
-   }
-   try{
-      BigInteger J = BigInteger("+");
-   }catch( std::invalid_argument& e ){
-      cout << e.what() << endl;
-      cout << "   continuing without interruption" << endl;
-   }
-   try{
-      BigInteger J = BigInteger("12329837492387492837492$4982379487293847");
-   }catch( std::invalid_argument& e ){
-      cout << e.what() << endl;
-      cout << "   continuing without interruption" << endl;
-   }
-   try{
-      BigInteger J = BigInteger("5298374902837409+82734098729287349827398");
-   }catch( std::invalid_argument& e ){
-      cout << e.what() << endl;
-      cout << "   continuing without interruption" << endl;
-   }
+//    // test exceptions
+//    try{
+//       BigInteger J = BigInteger("");
+//    }catch( std::invalid_argument& e ){
+//       cout << e.what() << endl;
+//       cout << "   continuing without interruption" << endl;
+//    }
+//    try{
+//       BigInteger J = BigInteger("+");
+//    }catch( std::invalid_argument& e ){
+//       cout << e.what() << endl;
+//       cout << "   continuing without interruption" << endl;
+//    }
+//    try{
+//       BigInteger J = BigInteger("12329837492387492837492$4982379487293847");
+//    }catch( std::invalid_argument& e ){
+//       cout << e.what() << endl;
+//       cout << "   continuing without interruption" << endl;
+//    }
+//    try{
+//       BigInteger J = BigInteger("5298374902837409+82734098729287349827398");
+//    }catch( std::invalid_argument& e ){
+//       cout << e.what() << endl;
+//       cout << "   continuing without interruption" << endl;
+//    }
 
-   cout << endl;
+//    cout << endl;
 
-   return EXIT_SUCCESS; */
+   return EXIT_SUCCESS; 
 }
