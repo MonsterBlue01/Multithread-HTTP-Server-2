cf133809b3777158c10de985d219c599f0d40eda
Felix Well <wdsrs020808@gmail.com>
2022-05-26 20:39:25 -0400

    5/26/22 5:39 PM

--- CSE 120/CSE 120 Homework 1.asm
+++ CSE 120/CSE 120 Homework 1.asm
@@ -1,2 +1,6 @@
 1. Just get the multiplication and get the addition. CPI = 0.2 * 1 
     + 0.4 * 2 + 0.1 * 3 + 0.3 * 4 = 2.5. IPC = 1 / 2.5 = 0.4.
+2. LD and other have the largest fractions. So we pick these two.
+3. Even removing the Other part doesn't achieve that amount of 
+    speedup. So it's not possible.
+    ; How to calculate the speedup?
