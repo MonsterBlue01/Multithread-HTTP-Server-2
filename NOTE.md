b74a3142011850fce83bf2b9c6e64a3ec2f9b7e4
MonstBlu <monstblu@Debian.MonstBlu>
2022-12-31 14:47:21 -0800

    2022/12/31 14:45

--- NOTE.md
+++ NOTE.md
@@ -4,4 +4,8 @@
 
 ## Future Work
 
+- Read the file and build a graph from the information in the file
+
+## Cancelled Work
+
 - Add function to delete nodes
--- graph.c
+++ graph.c
@@ -3,8 +3,25 @@
 
 #include "graph.h"
 
+int** initMatrix(int n) {
+    int** matrix = (int**)malloc(sizeof(int*) * n);
+    for (int i = 0; i < n; i++) {
+        matrix[i] = (int*)malloc(sizeof(int) * n);
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+    return matrix;
+}
+
+void deleteMatrix(int** matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
 int main() {
-    printf("Hello World!\n");
     FILE *fp = fopen("node.txt", "r");
     if (fp == NULL) {
         printf("File not found!\n");
@@ -19,5 +36,6 @@ int main() {
     }
     printf("max = %d\n", max);
     fclose(fp);
-    int matrix[max][max];
+    int** matrix = initMatrix(max);
+    deleteMatrix(matrix, max);
 }
