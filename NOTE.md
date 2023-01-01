44792aa02c704fa178c98afc53542f770b4efdce
MonstBlu <monstblu@debian.MonstBlu>
2023-01-01 14:43:16 -0800

    01/01/2023 2:45 PM

--- NOTE.md
+++ NOTE.md
@@ -2,10 +2,18 @@
 
 ## Previous Work
 
+- Read the file and build a graph from the information in the file
+
 ## Future Work
 
-- Read the file and build a graph from the information in the file
+- Since in this problem it doesn't make sense for a node to connect itself, two things need to change:
+    + When the program finds that there are nodes connected to itself in the input file, it skips directly, and at the end prompts the user that this has no meaning and has been skipped
+    + When printingMatrix, replace all the numbers in the opposite sides from the upper left to the lower right with -1 (that is, invalid)
 
 ## Cancelled Work
 
 - Add function to delete nodes
+
+## Important things to note:
+
+- The input document must follow a fixed format, otherwise the program will not have normal, expected results
--- graph.c
+++ graph.c
@@ -14,11 +14,42 @@ int** initMatrix(int n) {
     return matrix;
 }
 
+void createconnet(int** matrix, int a, int b) {
+    matrix[a - 1][b - 1] = 1;
+    matrix[b - 1][a - 1] = 1;
+}
+
 void deleteMatrix(int** matrix, int n) {
     for (int i = 0; i < n; i++) {
         free(matrix[i]);
     }
     free(matrix);
+}
+
+void readbuild(int** matrix, char* filename) {
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("File not found!\n");
+        exit(1);
+    }
+    int num1, num2;
+    while (fscanf(fp, "%d %d", &num1, &num2) != EOF) {
+        if (num1 == 0 && num2 == 0) {
+            break;
+        } else {
+            createconnet(matrix, num1, num2);
+        }
+    }
+    fclose(fp);
+}
+
+void printMatrix(int** matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main() {
@@ -37,5 +68,7 @@ int main() {
     printf("max = %d\n", max);
     fclose(fp);
     int** matrix = initMatrix(max);
+    readbuild(matrix, "node.txt");
+    printMatrix(matrix, max);
     deleteMatrix(matrix, max);
 }
