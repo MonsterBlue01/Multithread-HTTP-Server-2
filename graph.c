fa5a8aea3b6d2742d1a776ef35d92696cc8e6d21
Dongjing Wang <wdsrs020808@gmail.com>
2023-01-04 12:45:15 -0800

    01/04/2022 12:45 PM

--- graph.c
+++ graph.c
@@ -1,61 +1,63 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 #include "graph.h"
 
-int** initMatrix(int n) {
-    int** matrix = (int**)malloc(sizeof(int*) * n);
-    for (int i = 0; i < n; i++) {
-        matrix[i] = (int*)malloc(sizeof(int) * n);
-        for (int j = 0; j < n; j++) {
-            matrix[i][j] = 0;
-        }
-    }
-    return matrix;
-}
-
-void createconnet(int** matrix, int a, int b) {
-    matrix[a - 1][b - 1] = 1;
-    matrix[b - 1][a - 1] = 1;
-}
-
-void deleteMatrix(int** matrix, int n) {
-    for (int i = 0; i < n; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
-}
-
-bool readbuild(int** matrix, char* filename) {
-    bool same = false;
-    FILE *fp = fopen(filename, "r");
+void initializeGraph(Graph **g, char* filename) {
+    *g = (Graph*)malloc(sizeof(Graph));
+    int x, y;
+    (*g)->numEdges = 0, (*g)->numVertices = 0;
+    FILE* fp = fopen(filename, "r");
     if (fp == NULL) {
-        printf("File not found!\n");
+        perror("Error opening file");
         exit(1);
     }
-    int num1, num2;
-    while (fscanf(fp, "%d %d", &num1, &num2) != EOF) {
-        if (num1 == num2 && num1 != 0) {
-            same = true;
-        }
-        if (num1 == 0 && num2 == 0) {
-            break;
-        } else {
-            createconnet(matrix, num1, num2);
+    while (fscanf(fp, "%d%d", &x, &y) == 2) {
+        printf("%d %d\n", x, y);
+        (*g)->numVertices = fmax(fmax(x, y), (*g)->numVertices);
+        if (x != y) {
+            (*g)->numEdges++;
         }
     }
     fclose(fp);
-    return same;
 }
 
-void printMatrix(int** matrix, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i == j) {
-                printf("x ");
+void initializeAdjacencyMatrix(Graph *g, char* filename) {
+    g->adjacencyMatrix = (int**)malloc(g->numVertices * sizeof(int*));
+    for (int i = 0; i < g->numVertices; i++) {
+        g->adjacencyMatrix[i] = (int*)malloc(g->numVertices * sizeof(int));
+    }
+    for (int i = 0; i < g->numVertices; i++) {
+        for (int j = 0; j < g->numVertices; j++) {
+            g->adjacencyMatrix[i][j] = 0;
+        }
+    }
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        perror("Error opening file");
+        exit(1);
+    }
+    int x, y;
+    while (fscanf(fp, "%d%d", &x, &y) == 2) {
+        printf("%d %d\n", x, y);
+        if (x != y) {
+            g->adjacencyMatrix[x-1][y-1] = 1;
+            if (x != y) {
+                g->adjacencyMatrix[y-1][x-1] = 1;
+            }
+        }
+    }
+    fclose(fp);
+}
+
+void printMatrix(Graph *g) {
+    for (int i = 0; i < g->numVertices; i++) {
+        for (int j = 0; j < g->numVertices; j++) {
+            if (i != j) {
+                printf("%d ", g->adjacencyMatrix[i][j]);
             } else {
-                printf("%d ", matrix[i][j]);
+                printf("x ");
             }
         }
         printf("\n");
@@ -63,27 +65,10 @@ void printMatrix(int** matrix, int n) {
 }
 
 int main() {
-    FILE *fp = fopen("node.txt", "r");
-    if (fp == NULL) {
-        printf("File not found!\n");
-        exit(1);
-    }
-    int num;
-    int max = 0;
-    bool same;
-    while (fscanf(fp, "%d", &num) != EOF) {
-        if (num > max) {
-            max = num;
-        }
-    }
-    printf("max = %d\n", max);
-    fclose(fp);
-    int** matrix = initMatrix(max);
-    same = readbuild(matrix, "node.txt");
-    if (same == true) {
-        fprintf(stderr, "\x1b[35mwarn: \x1b[0m");
-        fprintf(stderr, "Although paths to the same node can be accepted, it doesn't make sense. You can choose to delete the path connected to the same node (for example, 3 3, 4 4) next time\n");
-    }
-    printMatrix(matrix, max);
-    deleteMatrix(matrix, max);
+    Graph *g = NULL;
+    initializeGraph(&g, "node.txt");
+    printf("Number of vertices: %d\n", g->numVertices);
+    printf("Number of edges: %d\n", g->numEdges);
+    initializeAdjacencyMatrix(g, "node.txt");
+    printMatrix(g);
 }
