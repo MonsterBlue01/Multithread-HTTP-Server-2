ce740e499c168ee96b0f089c5a2907b805116eb5
Blue Monster <73154603+MonsterBlue01@users.noreply.github.com>
2023-06-04 17:03:01 -0700

    Update finalcontroller_skel.py

--- Final Project/finalcontroller_skel.py
+++ Final Project/finalcontroller_skel.py
@@ -48,40 +48,14 @@ class Final (object):
         connection.addListeners(self)
 
     def do_final (self, packet, packet_in, port_on_switch, switch_id):
-        # Parse the IP and ICMP packets
-        ip = packet.find('ipv4')
-        icmp = packet.find('icmp')
+        msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match.from_packet(packet)
+        msg.idle_timeout = 30
+        msg.hard_timeout = 30
+        msg.actions.append(of.ofp_action_output(port = of.OFPP_ALL)) # Sends the packet out of all ports.
+        msg.data = packet_in
+        self.connection.send(msg)
 
-        if ip:
-            src_ip = str(ip.srcip)
-            dst_ip = str(ip.dstip)
-
-            # Rule: Untrusted Host cannot send ICMP traffic to Host 10 to 80, or the Server.
-            # Rule: Untrusted Host cannot send any IP traffic to the Server.
-            if src_ip == '106.44.82.103':
-                if icmp or dst_ip == '10.3.9.90':
-                    self.drop_packet(packet, packet_in)
-                    return
-
-            # ...50 to 80 in Department B, or the Server.
-            # Rule: Trusted Host cannot send any IP traffic to the Server.
-            if src_ip == '108.24.31.112':
-                if icmp and (dst_ip in ['10.2.5.50', '10.2.6.60', '10.2.7.70', '10.2.8.80'] or dst_ip == '10.3.9.90'):
-                    self.drop_packet(packet, packet_in)
-                    return
-                if dst_ip == '10.3.9.90':
-                    self.drop_packet(packet, packet_in)
-                    return
-
-            # Rule: Hosts in Department A (Host 10 to 40) cannot send any ICMP traffic to the hosts in
-            # Department B (Host 50 to 80), and vice versa.
-            if icmp and ((src_ip in ['10.1.1.10', '10.1.2.20', '10.1.3.30', '10.1.4.40'] and dst_ip in ['10.2.5.50', '10.2.6.60', '10.2.7.70', '10.2.8.80']) or
-                         (dst_ip in ['10.1.1.10', '10.1.2.20', '10.1.3.30', '10.1.4.40'] and src_ip in ['10.2.5.50', '10.2.6.60', '10.2.7.70', '10.2.8.80'])):
-                self.drop_packet(packet, packet_in)
-                return
-
-        # Default behavior: flood packet on all ports
-        self.flood_packet(packet, packet_in)
 
     def drop_packet (self, packet, packet_in):
         """
